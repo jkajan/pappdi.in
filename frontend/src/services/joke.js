d85be2c8902b9ebb73334bd8283ddd3f51eb6872
Simon Renlund <simon.renlund@aalto.fi>
2019-10-27 13:56:29 +0200

    Changed getAll function to async

--- frontend/src/services/joke.js
+++ frontend/src/services/joke.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/jokes'
 
-const getAll = () => {
-  const req = axios.get(baseUrl)
-  return req.then(res => res.data)
+const getAll = async() => {
+  const res = await axios.get(baseUrl)
+  return res.data
 }
 
 const create = async(newObject) => {
