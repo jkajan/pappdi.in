876121e705b55136468a781568db966d5054fe9e
Simon Renlund <simon.renlund@aalto.fi>
2019-11-07 16:20:01 +0200

    Added Basic Markov chain

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,14 +37,43 @@ const App = (props) => {
     setJoke([jokes[rand],rand])
   }
   //the function to generate a joke
-  const handleGen = () => {
-    console.log('NYI')
+  const markovMe = () => {
+    const markovChain = {}
+    const jokes = props.store.getState().joke
+    let jokestr = ""
+    for (let j = 0; j <jokes.length; j++) {
+      jokestr += jokes[j].content
+    }
+    const textArr = jokestr.split(' ')
+    for (let i = 0; i < textArr.length; i++) {
+      let word = textArr[i].toLowerCase().replace(/[\W_]/, "")
+      if (!markovChain[word]) {
+        markovChain[word] = []
+        }
+      if (textArr[i + 1]) {
+        markovChain[word].push(textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
+      }
+    }
+    const words = Object.keys(markovChain)
+    let word = words[Math.floor(Math.random() * words.length)]
+    let result = ''
+    let newWord = ''
+    for (let i = 0; i < words.length; i++ ) {
+      result += word + ' '
+      newWord =  markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
+      word = newWord
+      if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
+    }
+    console.log(result)
+    setJoke([{content: result,author: "Markov"},words.length])
+    console.log(joke)
   }
+
   return (
     <div className="container">
       <div className="header">Hello Papps! Välkommen till pappdi.in!</div>
       <div className="buttonContainer">
-        <button onClick={handleGen}>Generera ett 'Papp' skämt!</button>
+        <button onClick={markovMe}>Generera ett 'Papp' skämt!</button>
         <button onClick={handlePapp}>Random favorit i repris</button>
       </div>
       <Joke joke={joke[0]} />
