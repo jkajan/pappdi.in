4505ef6fdae772bed1a611cbb16abb289761cc9f
Simon Renlund <simon.renlund@aalto.fi>
2019-11-07 12:53:45 +0200

    First production build and initial depolyment

---
+++ backend/build/precache-manifest.c0f85e7d72d6441f104f4056e1846b86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2020a23e573d89e3b3a6cca75472939b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86d71383274112071100",
+    "url": "/static/css/main.1e65c8fd.chunk.css"
+  },
+  {
+    "revision": "a3004f7a9145060a9cad",
+    "url": "/static/js/2.ae552dbd.chunk.js"
+  },
+  {
+    "revision": "86d71383274112071100",
+    "url": "/static/js/main.880deb36.chunk.js"
+  },
+  {
+    "revision": "623c619b619fa6dacf60",
+    "url": "/static/js/runtime-main.5b450a31.js"
+  }
+]);
