31cf4e814b29a8de7248cde2623f17ce08b2cbc5
jkajan <jkajan@github.com>
2019-11-13 14:06:46 +0200

    Add certs

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const app = require('./app')
-const http = require('http')
+//const http = require('http')
 const config = require('./utils/config')
+const https = require('https'), fs = require('fs')
 
-const server = http.createServer(app)
+const options = {
+	//key: fs.readFileSync(config.KEY),
+	//cert: fs.readFileSync(config.CERT)
+	key: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/privkey.pem'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/fullchain.pem')
+}
+
+const server = https.createServer(options, app)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
