fb6276594c2aca317d51882bac80f5314fae9b40
Simon Renlund <simon.renlund@aalto.fi>
2019-11-14 17:23:59 +0200

    Made the server start with https only if NODE_ENV is in production mode

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 const app = require('./app')
-//const http = require('http')
 const config = require('./utils/config')
-const https = require('https'), fs = require('fs')
-
-const options = {
-	//key: fs.readFileSync(config.KEY),
-	//cert: fs.readFileSync(config.CERT)
-	key: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/privkey.pem'),
-	cert: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/fullchain.pem')
+if (config.NODE_ENV === 'production') {
+	const https = require('https')
+	const fs = require('fs')
+	const options = {
+		//key: fs.readFileSync(config.KEY),
+		//cert: fs.readFileSync(config.CERT)
+		key: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/privkey.pem'),
+		cert: fs.readFileSync('/etc/letsencrypt/live/pappdi.in/fullchain.pem')
+	}
+	const server = https.createServer(options, app)
+} else {
+	const http = require('http')
+	const server = http.createServer(app)
 }
-
-const server = https.createServer(options, app)
-
+console.log(`NODE_ENV = ${config.NODE_ENV}`)
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
