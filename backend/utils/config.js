67409f8ce9a7604852c86ac03c1c3997d4b7d6bf
Simon Renlund <simon.renlund@aalto.fi>
2019-11-15 15:42:44 +0200

    Added dev database

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,11 +3,9 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 let NODE_ENV = process.env.NODE_ENV
-/*
-if (process.env.NODE_ENV === 'test') {
+if (NODE_ENV === 'development') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
-*/
 module.exports = {
   MONGODB_URI,
   PORT,
